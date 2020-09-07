32457e7146224e81251ed74e5c9840c4ebdef46e
hfonseca2177 <hfonseca21@gmail.com>
2020-09-07 00:17:33 -0300

    First Commit

---
+++ Assets/Sphere.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public float fallMultiplier = 2.5f;    
+    private new Rigidbody rigidbody;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rigidbody.velocity.y > 0)
+        {
+            rigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+    }
+}
